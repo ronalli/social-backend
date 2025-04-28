ec1d936f5cb77409c6552335c7b6459e73679369
ronalli <khakimov.nikita.vch@gmail.com>
2025-04-28 19:55:23 +0300

    add new test by security

--- test/security/security.e2e-spec.ts
+++ test/security/security.e2e-spec.ts
@@ -4,6 +4,7 @@ import { initAppAndClearDB } from '../utils/base.init-settings';
 import { customRequest } from '../utils/custom-request';
 import { randomUUID } from 'node:crypto';
 import { serviceSecurity } from '../utils/security/service-security';
+import { jwtService } from '../../src/common/services/jwt.service';
 
 describe('Security', () => {
   let app: INestApplication;
@@ -111,4 +112,100 @@ describe('Security', () => {
 
     expect(resp.body.length).toBe(1);
   });
+
+  it('should correct delete one session by id', async () => {
+    const user = await serviceSecurity.registrationUser(app, 'bob');
+
+    const { refreshToken } = await serviceSecurity.authorizationUser(
+      app,
+      user.email,
+      user.password,
+    );
+
+    const decode = (await jwtService.decodeToken(refreshToken)) as {
+      deviceId: string;
+    };
+
+    await customRequest(app)
+      .delete(`security/devices/${decode.deviceId}`)
+      .set('Cookie', `refreshToken=${refreshToken}`)
+      .expect(204);
+
+    const resp = await customRequest(app)
+      .get('security/devices')
+      .set('Cookie', `refreshToken=${refreshToken}`)
+      .expect(200);
+
+    expect(resp.body.length).toBe(0);
+  });
+
+  it('should be delete only one session by id and all session return', async () => {
+    const user = await serviceSecurity.registrationUser(app, 'greg');
+
+    const value = [];
+
+    await Promise.all([
+      value.push(
+        await serviceSecurity.authorizationUser(app, user.login, user.password),
+      ),
+      value.push(
+        await serviceSecurity.authorizationUser(app, user.email, user.password),
+      ),
+      value.push(
+        await serviceSecurity.authorizationUser(app, user.login, user.password),
+      ),
+      value.push(
+        await serviceSecurity.authorizationUser(app, user.email, user.password),
+      ),
+    ]);
+
+    const decode = (await jwtService.decodeToken(value[0].refreshToken)) as {
+      deviceId: string;
+    };
+
+    await customRequest(app)
+      .delete(`security/devices/${decode.deviceId}`)
+      .set('Cookie', `refreshToken=${value[1].refreshToken}`)
+      .expect(204);
+
+    const resp = await customRequest(app)
+      .get('security/devices')
+      .set('Cookie', `refreshToken=${value[1].refreshToken}`)
+      .expect(200);
+
+    expect(resp.body.length).toBe(3);
+
+  })
+
+  it('should be return 403, when other user try to delete session by id ', async () => {
+
+    const user = await serviceSecurity.registrationUser(app, 'john');
+    const user1 = await serviceSecurity.registrationUser(app, 'alina');
+
+    const {refreshToken} = await serviceSecurity.authorizationUser(app, user.login, user.password);
+    const {refreshToken: rf2} = await serviceSecurity.authorizationUser(app, user1.login, user1.password);
+
+    const decode1 = (await jwtService.decodeToken(refreshToken)) as {
+      deviceId: string;
+    };
+
+    const decode2 = (await jwtService.decodeToken(rf2)) as {
+      deviceId: string;
+    };
+
+    await customRequest(app)
+      .delete(`security/devices/${decode1.deviceId}`)
+      .set('Cookie', `refreshToken=${rf2}`)
+      .expect(403);
+
+
+    await customRequest(app)
+      .delete(`security/devices/${decode2.deviceId}`)
+      .set('Cookie', `refreshToken=${refreshToken}`)
+      .expect(403);
+
+  })
+
 });
+
+//functions 73.66 lines 80.48
